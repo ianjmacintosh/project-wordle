6321254188702eaa3ab603d70bbaa372a50301a6
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-12-16 01:58:10 +0000

    Add tests for exercise 2

--- tests/skip/02-keeping-track-of-guesses.spec.js
+++
@@ -1,5 +0,0 @@
-/*
-- A new component should be created, to render previous guesses.
-- When the user submits their guess, that value should be rendered within this new component.
-- There should be no key warnings in the console!
-*/
