3104e03cb6c2117a75e99dd6edce0ac4e328ee95
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-12-16 21:24:39 +0000

    Update tests for exercise 4
    I also kind of give up on the tests at this point

--- tests/skip/04-game-logic.spec.js
+++
@@ -1,5 +0,0 @@
-/*
-- Import the `checkGuess` function from `/src/game-helpers.js`, and use it to validate each of the user's guesses
-- When rendering the letters in the `Guess` component, apply the letter's `status` to the `cell` element.
-- "Empty" guess slots should have the same markup as before: `<span class="cell"></span>`.
-*/
