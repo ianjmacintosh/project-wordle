4c07625be2ce936fad5f3b052bf9655e19e8bbe4
Ian J. MacIntosh <ianjmacintosh@users.noreply.github.com>
2025-12-16 21:24:52 +0000

    Solve exercise 4

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-function Guess({ value, id }) {
+import { checkGuess } from '../../game-helpers'
+
+function Guess({ value, id, answer }) {
   // Handle empty guesses by making an array of 5 "empty" letters
-  const letters = value.length === 0 ? Array(5).fill('')
+  const lettersInfo = value.length === 0 ? Array(5).fill({
+    letter: undefined,
+    status: undefined
+  })
     // Otherwise, make an array of the letters
-    : value.split('')
+    : checkGuess(value, answer)
 
   return (<p className="guess">
-    {letters.map((letter, index) => <span key={`${id}-letter_${index}`} className="cell">{letter}</span>)}
+    {lettersInfo.map(({ letter, status }, index) => {
+      return <span key={`${id}-letter_${index}`} className={`cell ${status ? status : ""}`.trim()}>{letter}</span>
+    }
+    )}
   </p>);
 }
 
